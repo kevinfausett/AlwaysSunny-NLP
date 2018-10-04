ea5666a5e6bd27b7b689b1057ace47c182ce13cf
Kevin Fausett <kevin.fausett@gmail.com>
2018-10-04 15:47:40 -0400

    Some more documentation

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ practicing NLP stuff with always sunny transcripts
 Features of the final version pushed: 
 * successfully generates a randomized transcript using 2-grams, randomly assigns speakers without regard to who is most likely to actually say it.
 * succcessfully scrapes every episode from SpringfieldSpringfield, parses them cleanly into a single text file with only words, newlines, and punctuation.
-* random scraps of code I didn't finish for scraping and parsing ForeverDreaming.
+* Scraps of code used in scraping the site with partially labeled transcripts are removed for readability. :(
 
 In the process of looking more closely at ForeverDreaming, I realized there wasn't a viable transcript for the things I wanted to do.
 Please see the spiritual successor to this project, FB-NLP, generating randomized quotes for every member of a facebook messenger chat/groupchat.
 Improvements of that version over this repo:
 * Divides quotes by person with a separate markov chain for every participant
 * Supports n-grams of any n
-* Much faster parsing into n-grams
+* Much faster parsing of final string
 * More NLP shenanigans to come.
--- main.py
+++ main.py
@@ -7,15 +7,14 @@ from random import choice
 
 def main():
     transcript = pathlib.Path("transcriptClean.txt").read_text()
-    transcriptRecleaned = transcript.replace("DENNIS: ", "").replace("MAC: ", "").replace("DEE: ", "").replace("Frank: ", "").replace("Charlie: ", "").replace("ALISON: ", "")
 
+    # Originally used: transcriptWords = nltk.word_tokenize(transcript)
     # Not appropriate for this task. It would split "didn't" into "did" and "n't" or "gonna"
     # into "gon" and "na" because it's tokenizing contractions separately, because they hold their own meaning.
-    # This is good for analysis, bad for word generation.
-    # transcriptWords = nltk.word_tokenize(transcript)
+    # This is good for analysis, bad for word generation. :(
 
     # Instead we'll just split on one or more spaces
-    words = re.split(' +', transcriptRecleaned)
+    words = re.split(' +', transcript)
 
     transitionTable = {}
 
@@ -26,13 +25,14 @@ def main():
         else:
             transitionTable[key] = [words[i]]
 
-    # dictionary.keys is an iterator now in Python 3. Rude
+    # dictionary.keys is an iterator now in Python 3.
     start = choice(list(transitionTable.keys()))
     word1, word2 = start[0], start[1]
     word3 = choice(transitionTable[start])
     cast = ['MAC: ', 'DENNIS: ', 'DEE: ', 'FRANK: ', "CHARLIE: "]
     firstSpeaker = choice(cast)
     res = firstSpeaker
+    # This is slow. Spiritual successor FB-NLP repo has a faster string building approach (join a list of strings.)
     res += (word1 + ' ' + word2 + ' ')
     current = firstSpeaker
     for i in range(0, 1000):
@@ -49,43 +49,9 @@ def main():
     pathlib.Path("myscript.txt").write_text(res, encoding="utf8")
     print(res)
 
-
-
-def scrapeForeverDreaming():
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-    headers = {'User-Agent': user_agent, 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Connection': 'Keep-Alive', 'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'en-US,*',}
-    page = requests.get("http://transcripts.foreverdreaming.org/viewtopic.php?f=104&t=30647&view=print", headers=headers, proxies={'http': '71.13.140.128:21776'})
-    megaTranscript = ""
-    megaDirtyTranscript = ""
-    allepisodes = []
-    for episode in allepisodes:
-        page = requests.get(episode, headers)
-        prettypage = BeautifulSoup(page.content, 'html.parser')
-        transcript = prettypage.find_all(class_="scrolling-script-container")
-        if transcript:
-            transcriptDirty = transcript[0].get_text()
-            transcriptClean = transcriptDirty.replace("\n", "").replace("\t", "").replace("\r", "").replace(" -", "\n")
-            megaDirtyTranscript += transcriptDirty
-            megaTranscript += transcriptClean
-            print(episode)
-            print(transcriptClean)
-        time.sleep(3)
-    pathlib.Path("transcriptClean.txt").write_text(megaTranscript, encoding="utf8")
-    pathlib.Path("transcriptDirty.txt").write_text(megaDirtyTranscript, encoding="utf8")
-
-
 def scrapeSpringfieldSpringfield():
-    #Use a proxy for forever dreaming
-
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
     headers = {'User-Agent': user_agent, 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Connection': 'Keep-Alive', 'Accept-Encoding': 'gzip, deflate', 'Accept-Language': 'en-US,*',}
-    # page = requests.get("https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=its-always-sunny-in-philadelphia&episode=s02e01", headers)
-    # prettypage = BeautifulSoup(page.content, 'html.parser')
-    # transcriptDirty = prettypage.find_all(class_="scrolling-script-container")[0].get_text()
-    # transcriptClean = transcriptDirty.replace("\n", "").replace("\t", "").replace("\r", "").replace(" -", "")
-    # print(transcriptClean)
-    headers = {'User-Agent': user_agent}
-
 
     s1to9e1to9 = ["https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=its-always-sunny-in-philadelphia&episode=s0" + str(season) + "e0" + str(episode) for season in range(1, 10) for episode in range(1, 10)]
     s1to9e10plus = ["https://www.springfieldspringfield.co.uk/view_episode_scripts.php?tv-show=its-always-sunny-in-philadelphia&episode=s0" + str(season) + "e" + str(episode) for season in range(1, 10) for episode in range(10, 16)]
